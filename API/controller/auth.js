6a3a6fa98630a4735a57fd36dd9f60975ceb750b
Vinh Hoang Cong <22026501@vnu.edu.vn>
2023-11-29 19:05:26 +0700

    move validate password to FE to hash

--- API/controller/auth.js
+++ API/controller/auth.js
@@ -8,67 +8,68 @@ import checkValidFullName from "../Utils/checkValidFullName.js";
 import checkUserNotExist from "../Utils/checkUserNotExist.js";
 
 export const login = async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const [user, fields] = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
-        if (user.length === 0) {
-            throw new Error("User is not exist");
-        }
-        console.log(user[0]);
-        const passwordHash = user[0].passWordHash;
-        console.log(passwordHash);
-        console.log(password);
-        if (!(await argon2.verify(passwordHash, password))) {
-            throw new Error("Wrong password or username");
-        }
-        
-        // const token = jwt.sign({ username: username }, process.env.JWT_SECRET);
-        // res.cookie("token", token, { httpOnly: true });
-
-        res.send(user[0].id.toString());
-    } catch (err) {
-        res.status(500).send(err.message);
+  const { username, password } = req.body;
+  try {
+    const [user, fields] = await pool.query(
+      "SELECT * FROM users WHERE username = ?",
+      [username]
+    );
+    if (user.length === 0) {
+      throw new Error("User is not exist");
     }
-}
+    console.log(user[0]);
+    const passwordHash = user[0].passWordHash;
+    console.log(passwordHash);
+    console.log(password);
+    if (!(await argon2.verify(passwordHash, password))) {
+      throw new Error("Wrong password or username");
+    }
 
+    // const token = jwt.sign({ username: username }, process.env.JWT_SECRET);
+    // res.cookie("token", token, { httpOnly: true });
+
+    res.send(user[0].id.toString());
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
 export const register = async (req, res) => {
-    const {username, email, password, fullName, gender} = req.body;
-    console.log(req.body);
-    try {
-        // check valid
-        await checkValidUsername(username);
-        await checkValidEmail(email);
-        await checkValidPassword(password);
-        await checkValidFullName(fullName);
-        await checkUserNotExist(username);
+  const { username, email, password, fullName, gender } = req.body;
+  console.log(req.body);
+  try {
+    // check valid
+    await checkValidUsername(username);
+    await checkValidEmail(email);
+    await checkValidPassword(password);
+    await checkValidFullName(fullName);
+    await checkUserNotExist(username);
 
-        let result = await pool.query("SELECT COUNT(*) as count FROM users");
-        let id = result[0][0].count + 1; // id = total number of users + 1
-        let passwordHash = await argon2.hash(password);
-        const data = [
-            id,
-            passwordHash,
-            username,
-            fullName,
-            email,
-            gender,
-            new Date(),
-        ];
-        await pool.query(
-            "INSERT INTO users (id, passwordHash, userName, fullName, email, gender) VALUES (?, ?, ?, ?, ?, ?)",
-            data);
-        res.send("Register successfully");
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send(err.message);
-    }
-}
+    let result = await pool.query("SELECT COUNT(*) as count FROM users");
+    let id = result[0][0].count + 1; // id = total number of users + 1
+    let passwordHash = await argon2.hash(password);
+    const data = [
+      id,
+      passwordHash,
+      username,
+      fullName,
+      email,
+      gender,
+      new Date(),
+    ];
+    await pool.query(
+      "INSERT INTO users (id, passwordHash, userName, fullName, email, gender) VALUES (?, ?, ?, ?, ?, ?)",
+      data
+    );
+    res.send("Register successfully");
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send(err.message);
+  }
+};
 
-export const logout = async (req, res) => {
-
-}
+export const logout = async (req, res) => {};
 
 export const test = async (req, res) => {
-    res.send("test");
-}
+  res.send("test");
+};
--- API/controller/post.js
+++ API/controller/post.js
@@ -1,22 +1,23 @@
 import pool from "../database.js";
 
 export const getPostsUserFollowing = async (req, res) => {
-    const { userId } = req.body;
-    try {
-        let command = 'SELECT * FROM posts INNER JOIN user_follow_user ON posts.ownerId = user_follow_user.userTargetId WHERE user_follow_user.userSourceId = (?);'
-        const [posts, fields2] = await pool.query(command, [userId]);        
-        res.send(posts);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-}
+  const { userId } = req.body;
+  try {
+    let command =
+      "SELECT * FROM posts INNER JOIN user_follow_user ON posts.ownerId = user_follow_user.userTargetId WHERE user_follow_user.userSourceId = (?);";
+    const [posts, fields2] = await pool.query(command, [userId]);
+    res.send(posts);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
 // export const getPostsOfUser = async (req, res) => {
 //     const { userId } = req.body;
 //     try {
 //         const userId = user[0].id;
 //         let command = 'SELECT * FROM posts WHERE ownerId = (?);'
-//         const [posts, fields2] = await pool.query(command, [userId]);        
+//         const [posts, fields2] = await pool.query(command, [userId]);
 //         posts.forEach(async (element) => {
 //             command = 'SELECT u.id FROM user_react_post INNER JOIN users AS u on user_react_post.userId = u.id where user_react_post.postId = (?);';
 //             const [usersReact, fields3] = await pool.query(command, [element.id]);
@@ -29,22 +30,18 @@ export const getPostsUserFollowing = async (req, res) => {
 // }
 
 export const addPost = async (req, res) => {
-    const { userId, contentImg, contentText } = req.body;
-    try {
-        const result = await pool.query("SELECT MIN(id + 1) as missing_id FROM posts WHERE id + 1 NOT IN (SELECT id FROM posts)");
-        const newPostId = result[0][0].missing_id;
-        const data = [
-            newPostId,
-            userId,
-            contentImg,
-            contentText,
-            new Date(),
-        ];
-        const command = 'INSERT INTO posts (id, ownerId, contentImg, contentText, createdAt) VALUES (?, ?, ?, ?, ?)';
-        await pool.query(command, data);
-        res.send("Post added successfully");
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-}
-
+  const { userId, contentImg, contentText } = req.body;
+  try {
+    const result = await pool.query(
+      "SELECT MIN(id + 1) as missing_id FROM posts WHERE id + 1 NOT IN (SELECT id FROM posts)"
+    );
+    const newPostId = result[0][0].missing_id;
+    const data = [newPostId, userId, contentImg, contentText, new Date()];
+    const command =
+      "INSERT INTO posts (id, ownerId, contentImg, contentText, createdAt) VALUES (?, ?, ?, ?, ?)";
+    await pool.query(command, data);
+    res.send("Post added successfully");
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
