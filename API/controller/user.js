caced74447f00ed99c2fd7acb6d094b77fceab68
Hoàng Công Vinh <haxorus5224@gmail.com>
2023-12-02 13:05:51 +0700

    re-position checkvalid password back to BE

--- API/controller/user.js
+++ API/controller/user.js
@@ -1,22 +1,21 @@
 import pool from "../database.js";
 
-
 /**
  * function to get user info
- * @param userId 
+ * @param userId
  * @returns object of user
  */
 export const getUserInfos = async (req, res) => {
-    const { userId } = req.body;
-    try {
-        const command = "SELECT * FROM users WHERE id = ?";
-        const [users] = await pool.query(command, [userId]);
-        users.forEach(element => {
-            delete element.passWordHash;
-        });
-        console.log(users);
-        res.send(users[0]);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
+  const { userId } = req.body;
+  try {
+    const command = "SELECT * FROM users WHERE id = ?";
+    const [users] = await pool.query(command, [userId]);
+    users.forEach((element) => {
+      delete element.passWordHash;
+    });
+    console.log(users);
+    res.send(users[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
 };
