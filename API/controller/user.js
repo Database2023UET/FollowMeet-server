812460c3d1d1ba5cfb65abdbb1c701234b9db547
hoangquocvietuet@gmail.com <hoangquocvietuet@gmail.com>
2023-12-02 12:32:44 +0700

    fixed user router

--- API/controller/user.js
+++ API/controller/user.js
@@ -1,16 +1,21 @@
 import pool from "../database.js";
 
+
+/**
+ * function to get user info
+ * @param userId 
+ * @returns object of user
+ */
 export const getUserInfos = async (req, res) => {
     const { userId } = req.body;
     try {
         const command = "SELECT * FROM users WHERE id = ?";
         const [users] = await pool.query(command, [userId]);
-        // Assuming that the query returns an array of users
-        const sanitizedUsers = users.map(user => {
-            const { passwordHash, ...sanitizedUser } = user;
-            return sanitizedUser;
+        users.forEach(element => {
+            delete element.passWordHash;
         });
-        res.send(sanitizedUsers);
+        console.log(users);
+        res.send(users[0]);
     } catch (err) {
         res.status(500).send(err.message);
     }
